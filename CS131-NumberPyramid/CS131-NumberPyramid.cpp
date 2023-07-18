c4c6c75d0bff490d24c4173ba807af8b8bafb147
TaylorBisset <87632154+TaylorBisset@users.noreply.github.com>
2023-07-18 11:56:07 -0700

    Add project files.

---
+++ CS131-NumberPyramid/CS131-NumberPyramid.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+/*
+Use embedded while loops to build the following number pyramid.
+What repeating patterns do you see? These you will make into loops
+     1
+    121
+   12321
+  1234321
+ 123454321
+12345654321
+*/
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
